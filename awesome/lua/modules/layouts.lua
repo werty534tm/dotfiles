3384e7da86f342cac7356dc6efaccc3d9719fe87
werty534tm <tomascanosanta@gmail.com>
2024-09-08 14:17:20 +0200

    Cambio los layout de awesomeWM

--- awesome/lua/modules/layouts.lua
+++ awesome/lua/modules/layouts.lua
@@ -2,8 +2,8 @@ local awful = require("awful")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local senos = {
-	awful.layout.suit.fair,
-	-- awful.layout.suit.tile,
+	-- awful.layout.suit.fair,
+	awful.layout.suit.tile,
 	awful.layout.suit.floating,
 	-- awful.layout.suit.tile.left,
 	-- awful.layout.suit.tile.bottom,
